45cdd89015bdef617177a727960d4cb9c5616823
ekanth24 <156081630+ekanth24@users.noreply.github.com>
2024-02-22 11:02:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -135,9 +135,9 @@ resource "aws_route_table_association" "b" {
 
 #Create EC2 Instance
 resource "aws_instance" "webserver1" {
-  ami                    = "ami-06b72b3b2a773be2b"
+  ami                    = "ami-0cf10cdf9fcd62d37"
   instance_type          = "t2.medium"
-  availability_zone      = "ap-south-1"
+  availability_zone      = "us-east-1"
   key_name               = "teform"
   vpc_security_group_ids = [aws_security_group.webserver-sg.id]
   subnet_id              = aws_subnet.web-subnet-1.id
